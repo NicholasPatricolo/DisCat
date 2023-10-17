94b106305ed6ec1ff95de185e4665f2e0b6fff60
Chimera <patricolonicholas@gmail.com>
2023-10-17 22:46:06 +0200

    Update catasto-discord.js
    - Spostamento della stringa per prendere il "risultato totale intestatari" ne footer dell'embed
    - let body = ""; //-->Variabile vuota tanto si va ad aggiungere al ciclo quando ricerca i dati!
    -> body += `NOME: ${intestatario.NOME}\nCOGNOME: ${intestatario.COGNOME}\nDATA: ${formatDate(intestatario.DATA)}\n\n`
    - Migliorata La grafica degli embed impostabile nel file di configurazione
    - e' avast!

--- catasto-discord.js
+++ catasto-discord.js
@@ -1,3 +1,25 @@
+/*
+#########################################################
+###                                                   ###
+###    PROGETTO DI ESTRAZIONE DATI CATASTALI          ###
+###                                                   ###
+#########################################################
+
+A PROPOSITO:
+QUESTO PROGETTO: è stato dedicato a fornire un meccanismo per l'accesso ai dati precedentemente archiviati in un database DBF e estratti tramite un interfaccia che andava sul sistema MsDos, che sono stati successivamente convertiti. Il risultato finale è un bot Discord che permette agli utenti di estrarre dati mediante comandi interattivi.
+QUESTO PROGETTO: si concentra sull'estrazione di dati da un database catastale ( LOCALE ( paricelle.json || anagrafica.json)).
+QUESTO PROGETTO: e' stato possibile grazie agli sforzi congiunti dei seguenti sviluppatori:
+
+- [Nicholas Patricolo] - (Ruolo: Conversione dati da DBF a JSON || Full stack discord bot dev.) 
+- [Manuel De Flavis] -   (Ruolo:  Full stack discord bot dev.)
+
+I dati estratti dal database catastale includono informazioni cruciali come le proprietà, i proprietari, i valori catastali, ecc.
+
+Riconoscimenti Database:
+
+- Nome del Database: [Inserisci il nome del tuo database]
+*/
+
 const Discord = require('discord.js');
 const config = require('./config.json');
 const JsonParticelle = require('./particelle.json');
@@ -34,10 +56,20 @@ client.on('message', (message) => {
         message.channel.send(embedIntestatriParticella(intest));
       }
       else{
-        message.reply('Nessun risultato trovato per FOGLIO e PARTICELLA specificati.')
+        //message.reply('Nessun risultato trovato per FOGLIO e PARTICELLA specificati.')
+        const embed = new Discord.MessageEmbed()
+      .setTitle('Nessun risultato trovato per FOGLIO e PARTICELLA specificati.')
+      .setColor('#ff0000')
+      .setThumbnail(config.img_errore);//https://icons.iconarchive.com/icons/icojam/blue-bits/256/database-check-icon.png
+       message.channel.send(embed);
       }
     } else {
-      message.reply('Utilizzo del comando errato. Usa `!p <FOGLIO> <PARTICELLA>`.');
+      //message.reply('Utilizzo del comando errato. Usa `!p <FOGLIO> <PARTICELLA>`.');
+      const embed = new Discord.MessageEmbed()
+      .setTitle('Utilizzo del comando errato. Usa `!p <FOGLIO> <PARTICELLA>')
+      .setColor('#ff0000')
+      .setThumbnail(config.img_errore);//https://icons.iconarchive.com/icons/icojam/blue-bits/256/database-check-icon.png
+       message.channel.send(embed);
     }
   }
   if (command === cmd_cerca_anagrafica) {
@@ -53,34 +85,49 @@ client.on('message', (message) => {
         message.channel.send(embedParticelle(partite));
       }
       else {
-        message.reply('Nessun risultato trovato per COGNOME e NOME specificati.');
+        //message.reply('Nessun risultato trovato per COGNOME e NOME specificati.');
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`Nessun risultato trovato per COGNOME e NOME specificati.`)
+        .setColor('#ff0000')
+        .setThumbnail(config.img_errore);
+         message.channel.send(embed);
       }
     } else {
-      message.reply('Utilizzo del comando errato. Usa `!a <COGNOME> <NOME>`.');
+      //message.reply('Utilizzo del comando errato. Usa `!a <COGNOME> <NOME>`.');
+      const embed = new Discord.MessageEmbed()
+      .setTitle('Utilizzo del comando errato. Usa `!a <COGNOME> <NOME>.')
+      .setColor('#ff0000')
+      .setThumbnail(config.img_errore);
+       message.channel.send(embed);
     }
   }
 });
 
+
+
+
+function embedDatiParticella(particella){
+  const embed = new Discord.MessageEmbed()
+  embed.setTitle(`INFORMAZIONI PARTICELLA: ${particella.NUMERO} FOGLIO: ${particella.FOGLIO}`);
+  let body = `ETTARI: ${particella.ETTARI}\nARE: ${particella.ARE}\nCENTIARE: ${particella.CENTIARE}\nCLASSE: ${particella.CLASSE}`;
+  embed.setDescription(body);
+  embed.setThumbnail(config.img_informazioni_particella);//-->ICONA INFORMAZIONI PARTICELLA
+  embed.setTimestamp();
+  return embed;
+}
+
 function embedIntestatriParticella(intestatari){
   const embed = new Discord.MessageEmbed()
   embed.setTitle("INTESTATARI PARTICELLA");
-  let body = "PROPRIETARI: " + intestatari.length +"\n\n";
+  let body = "";//-->Variabile vuota tanto si va ad aggiungere al ciclo quando ricerca i dati!
   intestatari.forEach(intestatario => {
     console.log(intestatario);
     body += `NOME: ${intestatario.NOME}\nCOGNOME: ${intestatario.COGNOME}\nDATA: ${formatDate(intestatario.DATA)}\n\n`
   })
   embed.setDescription(body);
   embed.setTimestamp();
-  embed.setFooter("Vai! ruba la legna!")
-  return embed;
-}
-
-function embedDatiParticella(particella){
-  const embed = new Discord.MessageEmbed()
-  embed.setTitle(`INFORMAZIONI PARTICELLA: ${particella.NUMERO} FOGLIO: ${particella.FOGLIO}`);
-  let body = `ETTARI: ${particella.ETTARI}\nARE: ${particella.ARE}\nCENTIARE: ${particella.CENTIARE}\n`;
-  embed.setDescription(body);
-  embed.setTimestamp();
+  embed.setThumbnail(config.img_intestatari_particella);
+  embed.setFooter('La proprietà di questa particella è condivisa tra : ' + intestatari.length + ' Prorprietari')
   return embed;
 }
 
@@ -104,6 +151,7 @@ function embedParticelle(partite){
   embed.setDescription(body);
   embed.setTimestamp();
   embed.setFooter("Totale particelle: " + totParticelle)
+  embed.setThumbnail(config.img_intestatari_particella_anagrafica);
   return embed;
 }
 
